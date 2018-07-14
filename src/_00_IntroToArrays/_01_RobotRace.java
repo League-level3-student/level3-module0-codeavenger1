c3b2b3c39b24189fb161b213873c9d1f3f192a69
codeavenger1 <league@WTS5.attlocal.net>
2018-07-14 14:27:25 -0700

    robot race

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -9,12 +9,16 @@ import org.jointheleague.graphical.robot.Robot;
 public class _01_RobotRace {
 	//1. make a main method
 	public static void main(String[] args) {
+		Random rand = new Random();
+		int winner;
 		//2. create an array of 5 robots.
 		Robot [] rob = new Robot[5]; 
 		//3. use a for loop to initialize the robots.
 		for (int i = 0; i < rob.length; i++) {
 			rob[i] = new Robot();
+			rob[i].setSpeed(10);
 		}
+		
 			//4. make each robot start at the bottom of the screen, side by side, facing up
 			rob[0].setX(70);
 			rob[0].setY(450);
@@ -29,11 +33,19 @@ public class _01_RobotRace {
 			
 		//5. use another for loop to iterate through the array and make each robot move 
 	    //   a random amount less than 50.
+			boolean go = true;
+			while (go) {
 			for (int i = 0; i < rob.length; i++) {
-				
-			}
+				rob[i].move(rand.nextInt(50));
+				if(rob[i].getY()< 0) {
+					go = false;
+					winner = i;
+					JOptionPane.showMessageDialog(null, "Robot " + winner+1 + " won!");
+					rob[winner].sparkle();
+				}
+			}}
 		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
+			
 		//7. declare that robot the winner and throw it a party!
     	
 		//8. try different races with different amounts of robots.
